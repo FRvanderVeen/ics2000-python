b3b76482bce035d6a73694d662a3a065bdda465c
Rutger de Graaf <rutger@whizzkit.nl>
2025-10-20 07:49:48 +0200

    Rename egg

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ics2000",
-    version="1.0.2",
+    name="ics2000-python",
+    version="1.0.3",
     author="Rutger de Graaf",
     author_email="",
     description="Trust ICS-2000 Python library",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/FRvanderVeen/ics2000-python",
+    url="https://github.com/rdegraafwhizzkit/ics2000-python",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
